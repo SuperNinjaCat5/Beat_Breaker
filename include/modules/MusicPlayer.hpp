9b30348851699ab40a67b91e3751b3fe2fd7c118
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-12-28 18:26:20 -0500

    Finish MusicPlayer class to properly encapsulate and update

--- include/modules/MusicPlayer.hpp
+++ include/modules/MusicPlayer.hpp
@@ -4,18 +4,26 @@
 class MusicPlayer {
 private:
   float timePositionMs;
-  bool hasSong = false;
-
-public:
   Music currentSong;
 
+public:
+  bool hasSong = false;
+  bool songPlaying = false;
+
+  // Special
   MusicPlayer(); // Constructor
+  bool assertSong();
 
   // Fetches
   float getTimePositionMs();
+  Music getCurrentSong();
 
   // Actions
   void startPlayer();
   void startPlayer(Music &song_);
+  void stopPlayer();
   void switchSong(Music &newSong);
+
+  // Main
+  void update();
 };
--- src/modules/MusicPlayer.cpp
+++ src/modules/MusicPlayer.cpp
@@ -2,23 +2,68 @@
 #include "raylib.h"
 #include <iostream>
 
-MusicPlayer::MusicPlayer() {}
-
-void MusicPlayer::startPlayer() {
+// Special
+MusicPlayer::MusicPlayer() {}; // Constructor
+bool MusicPlayer::assertSong() {
   if (hasSong) {
-    startPlayer(currentSong);
+    return true;
   } else {
-    std::cerr
-        << "Music player was started with a given song or current song to play";
+    std::cerr << "Music player function was called without a given song or "
+                 "current song to play";
+    return false;
   }
+};
+
+// Fetches
+float MusicPlayer::getTimePositionMs() {
+
+  assertSong();
+
+  float timePositionSec = GetMusicTimePlayed(currentSong);
+
+  float timePositionMs = timePositionSec / 1000.0f;
+
+  return timePositionMs;
+};
+
+Music MusicPlayer::getCurrentSong() {
+  assertSong();
+  return currentSong;
+};
+
+// Actions
+void MusicPlayer::startPlayer() {
+
+  assertSong();
+
+  startPlayer(currentSong);
 }
 
 void MusicPlayer::startPlayer(Music &song_) {
-  hasSong = true;
-
   currentSong = song_;
+
+  hasSong = true;
+  songPlaying = true;
 
   PlayMusicStream(currentSong);
 }
 
-void 
+void MusicPlayer::stopPlayer() {
+
+  assertSong();
+
+  songPlaying = false;
+
+  StopMusicStream(currentSong);
+}
+
+void MusicPlayer::switchSong(Music &newSong) {
+  currentSong = newSong;
+  hasSong = true;
+};
+
+// Main
+void MusicPlayer::update() { // Should be called once per frame
+  assertSong();
+  timePositionMs = getTimePositionMs();
+};
